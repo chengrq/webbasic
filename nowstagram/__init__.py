a45b0e856aea4ada6f53ef6c6388df9a669bb65d
chengrq <845839014@qq.com>
2019-02-06 21:34:06 +0800

    a

--- nowstagram/__init__.py
+++ nowstagram/__init__.py
@@ -3,8 +3,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 app = Flask(__name__)
-app.config.from_pyfile('app.conf.txt')
+app.config.from_pyfile('app.conf')
 db = SQLAlchemy(app)
 
 from nowstagram import views, models
--- nowstagram/views.py
+++ nowstagram/views.py
@@ -1,7 +1,10 @@
 # -*- encoding=UTF-8 -*-
 
 from nowstagram import app
+from nowstagram.models import Image, User
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    image = Image.query.order_by('id desc').limit(10).all()
+    return render_template('index.html',images=images)
