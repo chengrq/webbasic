09d9376aa2b100d295a2469e1f0e3094b094859e
chengrq <845839014@qq.com>
2019-02-07 16:46:50 +0800

    a

--- nowstagram/views.py
+++ nowstagram/views.py
@@ -1,10 +1,24 @@
 # -*- encoding=UTF-8 -*-
 
-from nowstagram import app
+from nowstagram import app,db
 from nowstagram.models import Image, User
-from flask import render_template
+from flask import render_template, redirect
 
 @app.route('/')
 def index():
     image = Image.query.order_by('id desc').limit(10).all()
-    return render_template('index.html',images=images)
+    return render_template('index.html',images=image)
+
+@app.route('/image/<int:image_id>/')
+def image(image_id):
+    image = Image.query.get(image_id)
+    if image == None:
+        return redirect('/')
+    return render_template('pageDetail.html', image = image)
+
+@app.route('/profile/<int:user_id>/')
+def profile(user_id):
+    user = User.query.get(user_id)
+    if user == None:
+        return redirect('/')
+    return render_template('profile.html', user = user)
